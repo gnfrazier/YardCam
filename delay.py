176ef711f87dbd5bf0fde7d2457e27252d4f6a7e
Greg <gnfrazier@gmail.com>
2017-01-01 08:40:26 -0500

    Added main variables

--- delay.py
+++ delay.py
@@ -1,3 +1,13 @@
 """Calcs delay for tracking sunrise and sunset"""
 
 import loc
+
+
+main():
+    lat = loc.lat()
+    lon = loc.lon()
+    wun = loc.wun()
+
+
+if __name__ == '__main__':
+    main()
