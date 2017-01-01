373fd1c3e2567fb5d4a455aa8749fafa05878282
Greg <gnfrazier@gmail.com>
2017-01-01 16:51:21 -0500

    refactored local refactored astro data call completed sunrise and sunset calls

--- delay.py
+++ delay.py
@@ -1,27 +1,53 @@
-"""Calcs delay for tracking sunrise and sunset"""
+"""Calcs delay for tracking sunrise and sunset.
+Requires local file of loc.py with Wunderground API key"""
 
 import loc
 import requests
+import requests
 
 
-def rise(lat, lon, alt, wun):
-
-    url = 'http://api.wunderground.com/api/' + wun + \
-        '/astronomy/q/' + lat + ',' + lon + '.json'
-
-    rdata = requests.get(url)
-    data = data.json()
-    # data.keys() returns keys
-    sunrise = (data['sun_phase']['sunrise'])  # returns dict of sunrise time
-
-    return data
-
-
-def main():
+def get_local():
     lat = loc.lat()
     lon = loc.lon()
     wun = loc.wun()
     alt = loc.alt()
+
+    return (lat, lon, alt, wun)
+
+
+def get_astro():
+    local = get_local()
+
+    url = 'http://api.wunderground.com/api/' + local[3] + \
+        '/astronomy/q/' + local[0] + ',' + local[1] + '.json'
+
+    rdata = requests.get(url)
+    data = rdata.json()
+    return data
+
+
+def sunrise(data=None):
+
+    if not data:
+        data = get_astro()
+    # data.keys() returns keys
+    sunrise = (data['sun_phase']['sunrise'])  # returns dict of sunrise time
+
+    return sunrise
+
+
+def sunset(data=None):
+
+    if not data:
+        data = get_astro()
+    # data.keys() returns keys
+    sunset = (data['sun_phase']['sunset'])  # returns dict of sunrise time
+
+    return sunset
+
+
+def main():
+
     rdata = rise(lat, lon, alt, wun)
 
     return rdata
